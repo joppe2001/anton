50c17511f429f0830ea10863b329bae5c695cfc0
joppe montezinos <joppe.montezinos@gmail.com>
2023-10-17 21:37:16 +0200

    lmao

--- components/molecules/ImageGrid.vue
+++ components/molecules/ImageGrid.vue
@@ -1,42 +1,50 @@
 <template>
-    <div class="wrapper">
-        <Swiper class="swiper-cards" :modules="[SwiperEffectCoverflow]" :slides-per-view="1" :loop="false"
-            :effect="'coverflow'" :centeredSlides="true"
-            :coverflow-effect="{ rotate: 50, stretch: 0, depth: 100, modifier: 1, slideShadows: false }">
-            <SwiperSlide v-for="element in assetGrid" :key="element?.url">
-                <div class="swiper-zoom-container grain">
-                    <NuxtImg :src="element?.url" :alt="element.__typeName" />
-                    <div class="img-text">
-                        <p>{{ element.name }}</p>
-                    </div>
-                </div>
-            </SwiperSlide>
-        </Swiper>
-    </div>
+  <div class="wrapper">
+    <Swiper
+      class="swiper-cards"
+      :modules="[SwiperEffectCoverflow]"
+      :slides-per-view="1"
+      :loop="false"
+      :effect="'coverflow'"
+      :centeredSlides="true"
+      :coverflow-effect="{
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: false,
+      }"
+    >
+      <SwiperSlide v-for="element in assetGrid" :key="element?.url">
+        <div class="swiper-zoom-container grain">
+          <NuxtImg :src="element?.url" :alt="element.__typeName" />
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  </div>
 </template>
 
 <script setup>
-
-const props = defineProps({
+  const props = defineProps({
     data: {
-        type: Object,
-        required: true,
+      type: Object,
+      required: true,
     },
     index: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
-});
+  });
 
-const assetGrid = computed(() => {
+  const assetGrid = computed(() => {
     return props.data.asset_grid;
-});
+  });
 
-console.log('grid', props.data);
+  console.log('grid', props.data);
 </script>
 
 <style scoped lang="scss">
-.wrapper {
+  .wrapper {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -45,62 +53,39 @@ console.log('grid', props.data);
 
     .swiper-container,
     .swiper-cards {
-        width: 30%;
-        height: 100%;
+      width: 30%;
+      height: 100%;
 
-        @media (max-width: 768px) {
-            width: 70%;
-        }
+      @media (max-width: 768px) {
+        width: 70%;
+      }
 
-        @media (max-width: 480px) {
-            width: 80%;
-        }
+      @media (max-width: 480px) {
+        width: 80%;
+      }
     }
 
     .swiper-slide {
-        text-align: center;
-        font-size: 18px;
+      text-align: center;
+      font-size: 18px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      .swiper-zoom-container {
+        height: 100%;
         display: flex;
         align-items: center;
         justify-content: center;
 
-        .swiper-zoom-container {
-            height: 100%;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-
-            &:hover .img-text {
-                opacity: 0;
-            }
+        &:hover .img-text {
+          opacity: 0;
         }
+      }
 
-        img {
-            object-fit: cover;
-        }
-
-        .img-text {
-            position: absolute;
-            height: 100%;
-            width: 100%;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            color: #fff;
-            opacity: 1;
-            transition: opacity 0.2s ease-in-out;
-
-            p {
-                padding: 0.5rem;
-                margin: 0;
-                font-size: 2rem;
-                font-weight: lighter;
-
-                @media (max-width: 768px) {
-                    font-size: 1.5rem;
-                }
-            }
-        }
+      img {
+        object-fit: cover;
+      }
     }
-}
+  }
 </style>
